c73715a00d2c0bda00aaf9c60efea2517dd0995b
islamHany <islamhany2268@gmail.com>
2022-08-24 21:12:15 +0200

    webpack prod config and set github workflows

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,17 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/, //any file that ends with mjs or js
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],//process all react jsx and all es6...15 and convert it to es5
-                        plugins: ['@babel/plugin-transform-runtime'], //enable additional features inside the browser like async await
-                    }
-                },
-            }
-        ] 
-    }
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, //any file that ends with mjs or js
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"], //process all react jsx and all es6...15 and convert it to es5
+            plugins: ["@babel/plugin-transform-runtime"], //enable additional features inside the browser like async await
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
