e1364264c629aa80a5ef1fb3c21859a48fa2cef4
islamHany <islamhany2268@gmail.com>
2022-08-28 12:39:13 +0200

    add public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest' //when HtmlWebpackPlugin tries to refer to some js file that has been created (takes the file name and prepend them with the public path)
     },
     plugins: [
         new ModuleFederationPlugin({
